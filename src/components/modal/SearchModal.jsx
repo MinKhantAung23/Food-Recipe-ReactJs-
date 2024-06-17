ab0b33306d21bd56501c3b08c1b8a0e8ea1a7e70
Min Khant Aung <minkhantaung743@gmail.com>
2024-06-17 20:46:30 +0630

    update code

--- src/components/modal/SearchModal.jsx
+++ src/components/modal/SearchModal.jsx
@@ -94,6 +94,7 @@ const SearchModal = () => {
                 type="button"
                 className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
                 data-hs-overlay="#hs-scroll-inside-body-modal"
+                onClick={handleCancel}
               >
                 Close
               </button>
